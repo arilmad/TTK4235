5906c7e62aef4e47463c1c933b03b5f72ede695a
arilmad <arilmad@stud.ntnu.no>
2018-02-28 11:06:36 +0100

    Hjelpes

--- main.c
+++ main.c
@@ -15,6 +15,8 @@ int main(){
 	int requested_floor;
 	int is_door_open;
 	int new_order;
+	int current_floor;
+
 	
 
 
@@ -25,8 +27,18 @@ int main(){
 		//mottar ordre og setter lamper. Returnerer 1 hvis ny ordre plukkes opp
 		new_order = receive_orders();
 		stop_button = elev_get_stop_signal();	
-		requested_floor = pending_orders() + 1;
+		
 		is_door_open = elev_get_door_open_lamp;
+		current_floor = elev_get_floor_sensor_signal;
+
+		if(current_floor != -1){
+
+			fsm_update_state_machine(current_floor);
+		}
+
+		else if(stop_button){
+			
+		}
 
 
 
---
+++ orders.c
@@ -0,0 +1,134 @@
+#include "orders.h"
+
+static int ORDERS[N_FLOORS][N_BUTTONS] = 
+{
+    { 0, 0, 0 },
+    { 0, 0, 0 },
+    { 0, 0, 0 },
+    { 0, 0, 0 },
+};
+int get_order_from_floor(int floor)
+{
+	for (int button = 0; button < N_BUTTONS; button++)
+	{
+		if(get_order(floor, button))
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+int get_order(int floor, int button)
+{
+	return ORDERS[floor][button];
+}
+
+int receive_orders(void)
+{
+	for (int floor = 0; floor < N_FLOORS; floor++)
+	{
+		for (int button = 0; button < N_BUTTONS; button++)
+		{
+			if ((floor == (N_FLOORS - 1)) && (button == 0))
+			{
+				continue;
+			}
+			else if ((floor == 0) && (button == 1))
+			{
+				continue;
+			}
+			else
+			{
+				
+				if(elev_get_button_signal(button, floor))
+				{
+					ORDERS[floor][button] = 1;
+					elev_set_button_lamp(button, floor, 1);
+					return 1;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+int button_held_down_in_floor(int floor)
+{
+	for (int button = 0; button < N_BUTTONS; button++)
+	{
+		if ((floor == (N_FLOORS - 1)) && (button == 0))
+		{
+			continue;
+		}
+		else if ((floor == 0) && (button == 1))
+		{
+			continue;
+		}
+		else
+		{
+			
+			if(elev_get_button_signal(button, floor))
+			{
+				return 1;
+			}
+		}
+	}
+
+	return 0;
+
+}
+
+
+
+int pending_orders(void)
+{
+	for (int floor = 0; floor < N_FLOORS; floor++)
+	{
+		for (int button = 0; button < N_BUTTONS; button++)
+		{
+			if (!((floor == (N_FLOORS - 1)) && (button == 0))&&(!(((floor == 0) && (button == 1)))))
+			{
+
+				if(ORDERS[floor][button])
+				{
+					//printf("pending_orders FANT pending orders!");
+					return floor;
+				}
+			} 
+		}
+	}
+	//printf("pending_orders fant INGEN pending orders!");
+	return -1;
+}
+
+void clear_order(int floor)
+{
+	for (int button = 0; button < N_BUTTONS; button++)
+	{
+		printf("clear_order kjører \n");
+		printf("floor: ");
+		printf("%d\n", floor);
+		if ((floor == (N_FLOORS - 1)) && (button == 0))
+		{
+			continue;
+		}
+		else if ((floor == 0) && (button == 1))
+		{
+			continue;
+		}
+		else
+		{
+			ORDERS[floor][button] = 0;
+			elev_set_button_lamp(button, floor, 0);
+		}
+	}
+}
+
+void clear_all_orders(void)
+{
+	for (int floor = 0; floor < N_FLOORS; floor++)
+	{
+		printf("clear_all_orders kjører \n");
+		clear_order(floor);
+	}
+}
